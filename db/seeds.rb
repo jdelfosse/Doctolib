a75f441d338f508b89a3e74a794ad663460897ee
jerome.delfosse <jerome.delfosse.78@gmail.com>
2018-10-26 02:42:53 +0200

    maj seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+#10.times do |index|
+#  doctor = Doctor.create!(last_name: "prenom#{index}",first_name:  "nom#{index}",specialty: "specialty#{index}",postal_code: "7812#{index}")
+#end
+
+#10.times do |index|
+#  patien = Patient.create!(last_name: "prenom#{index}",first_name:  "nom#{index}")
+#end
+
+#10.times do |index|
+#	appoitment = Appoitment.create!(id: "#{index}",  doctors_id: "#{index}", patients_id: "#{index}")
+#end
